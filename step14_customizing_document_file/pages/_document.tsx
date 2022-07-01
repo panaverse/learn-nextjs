3fb44624e1b128168fd43ca3db0855b700f586d3
Zia Khan <ziaukhan@hotmail.com>
2022-07-02 00:17:34 +0500

    add documentcontext type in 14

--- step14_customizing_document_file/pages/_document.tsx
+++ step14_customizing_document_file/pages/_document.tsx
@@ -1,8 +1,8 @@
-import Document,{ Html, Head, Main, NextScript} from 'next/document';
+import Document,{ Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 class MyDocument extends Document {
 
-    static async getInitialProps(ctx) {
+    static async getInitialProps(ctx: DocumentContext) {
         const initialProps = await Document.getInitialProps(ctx);
         return { ...initialProps };
     }
