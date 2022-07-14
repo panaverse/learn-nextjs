b3e1387f2e18e9062d8a2b61a9b14d2ae383832e
Zia Khan <ziaukhan@hotmail.com>
2022-07-14 13:34:17 +0500

    add step 33

---
+++ step33_chakra_ui_framer_motion/pages/_app.tsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <ChakraProvider><Component {...pageProps} /></ChakraProvider>
+}
+
+export default MyApp
---
+++ step33_chakra_ui_framer_motion/pages/index.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from 'next';
+import { Box, Container, keyframes } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+
+const animationKeyframes = keyframes`
+  0% { transform: scale(1) rotate(0); border-radius: 20%; }
+  25% { transform: scale(2) rotate(0); border-radius: 20%; }
+  50% { transform: scale(2) rotate(270deg); border-radius: 50%; }
+  75% { transform: scale(1) rotate(270deg); border-radius: 50%; }
+  100% { transform: scale(1) rotate(0); border-radius: 20%; }
+`;
+
+const animation = `${animationKeyframes} 2s ease-in-out infinite`;
+
+const Home: NextPage = () => {
+  return (
+    <Container h="100vh" display="flex" alignItems="center" justifyContent="center">
+      <Box
+        as={motion.div}
+        animation={animation}
+        // not work: transition={{ ... }}
+        padding="2"
+        // @ts-ignore - "Does not exist" Type Error against Motion
+        bgGradient="linear(to-l, #7928CA, #FF0080)"
+        width="12"
+        height="12"
+        display="flex"
+      />
+    </Container>
+  )
+}
+
+export default Home
