58d5db0ef6e75958395ab58fbe25c4338607623b
Zia Khan <ziaukhan@hotmail.com>
2022-11-22 10:43:40 +0500

    removed steps

--- step09_styling/tailwindCSS/step06_text_align/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="text-center">Text - Center</h1>
-      <h1 className="text-left">Text - Left</h1>
-      <h1 className="text-right">Text - Right</h1>
-    </>
-  );
-}
