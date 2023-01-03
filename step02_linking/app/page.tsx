2c01434d6c210ca104a30aeb03e3c16c38f0d50a
Zia Khan <ziaukhan@hotmail.com>
2023-01-03 12:44:54 +0500

    update step02

--- step02_linking/app/page.tsx
+++ step02_linking/app/page.tsx
@@ -1,4 +1,9 @@
-import Link from 'next/link';
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import Link from 'next/link'
+//import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
