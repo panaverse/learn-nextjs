2385986096dddf13e9fc9c55a8af395f44079e4b
Zia Khan <ziaukhan@hotmail.com>
2023-01-28 06:40:16 +0500

    add step 09

---
+++ step09_fetching_data_server_dynamic/README.md
@@ -0,0 +1,6 @@
+# Fetching Dynamic on the Server in Next.js 13
+
+[Dynamic Data Fetching](https://beta.nextjs.org/docs/data-fetching/fetching#dynamic-data-fetching)
+
+
+
---
+++ step09_fetching_data_server_dynamic/app/page.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+//import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+async function getData() {
+  const res = await fetch('https://api.quotable.io/random?tags=technology',
+  { cache: 'no-store' });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+
+  const quote = await getData();
+  
+  return (
+    <div>{quote.content}</div>
+  )
+}
