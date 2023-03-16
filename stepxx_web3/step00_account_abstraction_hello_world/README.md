76b19c1aad5e3ea790e3b7bc441b1bc4844601d5
Waris Hasan <53475608+warishasan@users.noreply.github.com>
2023-03-16 17:34:18 +0500

    added step for web3

---
+++ stepxx_web3/step00_account_abstraction_hello_world/README.md
@@ -0,0 +1,4 @@
+[Stackup quick start](https://docs.stackup.sh/docs/guides/quickstart)
+
+[Zerodev docs](https://docs.zerodev.app/)
+
---
+++ stepxx_web3/step00_account_abstraction_hello_world/src/app/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      hello world
+    </div>
+  )
+}
