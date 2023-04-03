60ad76d4dfc334f0d56a22a55b1e51fbca454bd1
Zia Khan <ziaukhan@hotmail.com>
2023-04-03 15:14:19 +0500

    middleware step

--- step11_route_handler/step04_middleware/readme.md
+++ step11_route_handler/step04_middleware/readme.md
@@ -1,3 +1,5 @@
 # Middleware
 
 [Middleware](https://nextjs.org/docs/advanced-features/middleware)
+
+    http://localhost:3000/about
---
+++ step11_route_handler/step04_middleware/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+}
