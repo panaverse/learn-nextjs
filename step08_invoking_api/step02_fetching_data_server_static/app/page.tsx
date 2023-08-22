2b402bb3e1a6e46393fc8601cc380a2c50c8ae1c
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-08-22 17:59:38 +0500

    step 2 improved

--- step08_invoking_api/step02_fetching_data_server_static/app/page.tsx
+++ step08_invoking_api/step02_fetching_data_server_static/app/page.tsx
@@ -1,28 +1,21 @@
-{/* @ts-expect-error Async Server Component */}
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
 
 async function getData() {
-  const res = await fetch('https://api.quotable.io/random?tags=technology');
+  const res = await fetch("https://api.quotable.io/random?tags=technology");
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
   // Recommendation: handle errors
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
 
   return res.json();
 }
 
 export default async function Home() {
-
   const quote = await getData();
-  
-  return (
-    <div>{quote.content}</div>
-  )
+
+  return <div>{quote.content}</div>;
 }
