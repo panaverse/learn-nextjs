79749023eb0eeb841eef3a2e062d9060b5608f4d
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-08-29 17:23:23 +0500

    route handler step 1 cleaned

--- step11_route_handler/step00_helloworld/app/api/hello/route.ts
+++ step11_route_handler/step00_helloworld/app/api/hello/route.ts
@@ -2,12 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const url = request.nextUrl;
-  if(url.searchParams.has("name")){
+
+  if (url.searchParams.has("name")) {
     const name = url.searchParams.get("name");
-    return new NextResponse('Hello, from: ' + name);
-  }
-  else {
+    return new NextResponse("Hello, from: " + name);
+  } else {
     return new NextResponse('Please send your name in search parameter "name"');
   }
-  
 }
