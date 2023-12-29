07b24cab638002a26b735a49b644b4c71a2b75de
mkdeveloper <khubaib.mk@gmail.com>
2023-12-29 10:33:41 +0500

    parallel routes example added

---
+++ step08_parallel_routes/src/app/@analytics/page.tsx
@@ -0,0 +1,8 @@
+const Analytics = async () => {
+  return (
+    <div className="flex max-w-lg rounded-md w-full h-56 bg-blue-300 justify-center items-center">
+      Analytics
+    </div>
+  );
+};
+export default Analytics;
---
+++ step08_parallel_routes/src/app/page.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <div className="flex max-w-5xl rounded-md w-full h-56 bg-red-300 justify-center items-center">
+      Home
+    </div>
+  );
+};
+export default Home;
